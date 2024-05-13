29bc692f3747117f1a2edc13cfe74f8e758bd7d0
Cologero <lennonalvarez530@gmail.com>
2024-05-13 12:30:15 -0700

    save

---
+++ Final Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rangeValue = 5f;
+    private Vector3 _startingPosition;
+    [SerializeField] private bool _isAttacking;
+    [SerializeField] private Transform _player;
+  
+    [SerializeField] private Rigidbody _enemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDirection = (_player.position - transform.position).normalized;
+        
+        float distance = Vector3.Distance(_player.position, transform.position);
+        //Debug.Log("Distance" + distance);
+        if(!_isAttacking)
+        {
+            if(distance < rangeValue)
+            {
+                _enemyRb.velocity = movementDirection * speed;
+                transform.LookAt(_player);
+            }
+            else
+            {
+                //_enemyRb.velocity = movementDirection * 0;
+            }
+        }
+    }// end Update
+
+    public void EnemyAttack()
+    {
+        _isAttacking = true;
+    
+        StartCoroutine("EnemyAttackCoolDown");
+    }
+
+    IEnumerator EnemyAttackCoolDown()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _isAttacking = false;
+    }
+}
